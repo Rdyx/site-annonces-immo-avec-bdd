3b3de940576a5738e45e00183f171e5801451302
Rdyx <tiranico.pro@gmail.com>
2017-08-22 16:11:24 +0200

    exo ok perso

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+	<div class="container">
+		<div class="col-xs-12 text-center">
+			<h1>Welcoume to Le Bon Coin Coin !</h1>
+		</div>
+	</div>
+	<div class="container text-center">
+		<div class="col-xs-2">
+			<ul class="list-unstyled">
+				<li><a href="index.php">Index</a></li>
+				<li><a href="rubriques.php">Rubriques</a></li>
+				<li><a href="annonces.php">Annonces</a></li>
+				<li><a href="annonceurs.php">Annonceurs</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-10">
+			<h2 class="title">Bienvenue !</h2>
+			<p>Bienvenue sur <strong>Le Bon Coin Coin</strong> cher visiteur !</p>
+		</div>
+	</div>
+	<div class="container text-center">
+		<div class="col-xs-12">
+			<h3>"J'en ai rien à footer"</h3>
+		</div>
+	</div>
+</body>
+</html>
